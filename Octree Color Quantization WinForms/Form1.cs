a4ceb29926ccb1fb663ab33a4f9dbb903b4a12a7
an2508374 <an2508374@gmail.com>
2023-12-02 20:51:41 +0100

    Add project files.

---
+++ Octree Color Quantization WinForms/Form1.cs
@@ -0,0 +1,10 @@
+namespace Octree_Color_Quantization_WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
